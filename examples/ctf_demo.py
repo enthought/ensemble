09635fedcc51c1aa4af3ba3f80e2a0c05d00aa96
John Wiggins <jwiggins@enthought.com>
2014-10-08 15:42:34 -0500

    Add CtfManager usage to the CTF example code.

--- examples/ctf_demo.py
+++ examples/ctf_demo.py
@@ -7,13 +7,17 @@ colors at the end points are editable, but cannot be removed.
 
 """
 
+from os.path import join
+
 from traits.etsconfig.api import ETSConfig
 ETSConfig.toolkit = 'qt4'
 
 from enaml.qt.qt_application import QtApplication
 from ensemble.ctf.editor import CtfEditor
+from ensemble.ctf.manager import CtfManager
 from ensemble.ctf.gui_utils import get_color
 import traits_enaml
+
 
 if __name__ == "__main__":
     with traits_enaml.imports():
@@ -21,8 +25,11 @@ if __name__ == "__main__":
 
     app = QtApplication()
 
-    ctf = CtfEditor(prompt_color_selection=get_color)
-    win = CtfDemoWindow(ctf=ctf)
+    ctf_editor = CtfEditor(prompt_color_selection=get_color)
+    ctf_manager = CtfManager.from_directory(
+        join(ETSConfig.application_data, 'CTF_demo')
+    )
+    win = CtfDemoWindow(ctf_editor=ctf_editor, ctf_manager=ctf_manager)
     win.show()
 
     app.start()
