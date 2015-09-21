81c4731a524538bb02560b0ae11b7250b3d748c3
Deepak Surti <dsurti@enthought.com>
2015-09-21 14:16:24 +0530

    Store number of points in bounding box

--- ensemble/volren/volume_bounding_box.py
+++ ensemble/volren/volume_bounding_box.py
@@ -1,3 +1,4 @@
+from traits.api import Int
 from tvtk.api import tvtk
 from tvtk.common import configure_input_data
 
@@ -9,6 +10,9 @@ class VolumeBoundingBox(ABCVolumeSceneMember):
     Volume.
     """
 
+    # The number of points should be 8 after the bounding box is computed
+    number_of_points = Int(0)
+
     #--------------------------------------------------------------------------
     # ABCVolumeSceneMember interface
     #--------------------------------------------------------------------------
@@ -19,6 +23,7 @@ class VolumeBoundingBox(ABCVolumeSceneMember):
         outline = tvtk.OutlineFilter()
         configure_input_data(outline, volume_actor.mapper.input)
         outline.update()
+        self.number_of_points = outline.output.number_of_points
         outline_mapper = tvtk.PolyDataMapper()
         configure_input_data(outline_mapper, outline.output)
         outline_actor = tvtk.Actor(mapper=outline_mapper)
