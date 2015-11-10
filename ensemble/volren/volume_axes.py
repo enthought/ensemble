3b91c8d60b271b60f7b4543fd0858b9a23065ba5
Pietro Berkes <pberkes@enthought.com>
2015-11-10 09:03:56 +0000

    Expose axis properties.

--- ensemble/volren/volume_axes.py
+++ ensemble/volren/volume_axes.py
@@ -1,4 +1,4 @@
-from traits.api import Bool, Float, Tuple
+from traits.api import Bool, Enum, Float, Str, Tuple
 from tvtk.api import tvtk
 
 from .volume_scene_member import ABCVolumeSceneMember
@@ -20,6 +20,16 @@ class VolumeAxes(ABCVolumeSceneMember):
     # Which axes should have a scale shown?
     visible_axis_scales = Tuple(Bool, Bool, Bool)
 
+    # Axes titles.
+    axis_titles = Tuple(Str, Str, Str)
+
+    # Axes units.
+    axis_units = Tuple(Str, Str, Str)
+
+    # Fly mode, determining the position of the axes on the bounding box.
+    fly_mode = Enum(('static_triad', 'closest_triad', 'furthest_triad',
+                     'outer_edges', 'static_edges'))
+
     #--------------------------------------------------------------------------
     # ABCVolumeSceneMember interface
     #--------------------------------------------------------------------------
@@ -31,13 +41,15 @@ class VolumeAxes(ABCVolumeSceneMember):
             bounds = volume_actor.bounds
             x_vis, y_vis, z_vis = self.visible_axis_scales
             x_range, y_range, z_range = self.visible_axis_ranges
+            x_title, y_title, z_title = self.axis_titles
+            x_units, y_units, z_units = self.axis_units
             cube_axes = tvtk.CubeAxesActor(
                 bounds=bounds,
                 camera=scene_model.camera,
                 tick_location='outside',
-                x_title='', x_units='',
-                y_title='', y_units='',
-                z_title='', z_units='',
+                x_title=x_title, x_units=x_units,
+                y_title=y_title, y_units=y_units,
+                z_title=z_title, z_units=z_units,
                 x_axis_visibility=x_vis,
                 y_axis_visibility=y_vis,
                 z_axis_visibility=z_vis,
@@ -47,6 +59,7 @@ class VolumeAxes(ABCVolumeSceneMember):
                 x_axis_minor_tick_visibility=self.show_axis_minor_ticks,
                 y_axis_minor_tick_visibility=self.show_axis_minor_ticks,
                 z_axis_minor_tick_visibility=self.show_axis_minor_ticks,
+                fly_mode=self.fly_mode,
             )
             scene_model.renderer.add_actor(cube_axes)
 
